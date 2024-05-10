dd47c45d819ed1f4833072c6cf99dff8337d7a8c
azuliate <harshbajwaw@gmail.com>
2024-05-10 13:14:10 -0700

    Added gravity and jumping

--- viral-game/Assets/Scripts/PlayerMovement.cs
+++ viral-game/Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,47 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    /// <summary>
+    /// Movement Variables
+    /// </summary>
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //Check if we are touching the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask.value);
+
+        //Reset velocity
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        //Getting w,a,s,d inputs
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
