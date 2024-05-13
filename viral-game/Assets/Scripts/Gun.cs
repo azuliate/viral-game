8aad58721557f8f031e7629e863a0b217fbecaa5
azuliate <harshbajwaw@gmail.com>
2024-05-13 16:36:28 -0700

    Added particles for impact.

--- viral-game/Assets/Scripts/Gun.cs
+++ viral-game/Assets/Scripts/Gun.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [Header("Objects")]
     public Camera fpsCamera;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
 
+    [Header("Stats")]
     public float damage = 10f;
     public float range = 100f;
 
+    [Header("Keybinds")]
     public KeyCode shootKey = KeyCode.Mouse0;
 
     // Update is called once per frame
@@ -24,16 +28,21 @@ public class Gun : MonoBehaviour
     {
         muzzleFlash.Play();
         RaycastHit hit;
+
+        //Raycast to enemy
         if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
-
+            
             Enemy enemy = hit.transform.GetComponent<Enemy>();
 
             if(enemy != null)
             {
                 enemy.TakeDamage(damage);
             }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
